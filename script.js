3a75829499f72e79dddeae413cbce11e22910109
huthoinguyn <huthoi99@gmail.com>
2022-08-23 14:05:55 +0700

    fix Cursor Hover

--- script.js
+++ script.js
@@ -2,10 +2,31 @@ const $ = document.querySelector.bind(document),
   $$ = document.querySelectorAll.bind(document)
 const app = {
   handleEvents: function() {
-    document.onmousemove = (e) => {
-      $("#cursor").style.top = e.clientY + "px"
-      $("#cursor").style.left = e.clientX + "px"
+    const cursor = $("#cursor")
+    let mouseX = 0,
+      mouseY = 0,
+      ballX = 0,
+      ballY = 0,
+      speed = 0.2
+
+    function animate() {
+      let distX = mouseX - ballX
+      let distY = mouseY - ballY
+
+      ballX = ballX + distX * speed
+      ballY = ballY + distY * speed
+
+      cursor.style.left = ballX + "px"
+      cursor.style.top = ballY + "px"
+
+      requestAnimationFrame(animate)
     }
+    animate()
+
+document.addEventListener("mousemove", function(event){
+  mouseX = event.pageX;
+  mouseY = event.pageY;
+})
     const toggle = $('.toggle')
     const sidebar = $('.sidebar')
     toggle.onclick = () => {
